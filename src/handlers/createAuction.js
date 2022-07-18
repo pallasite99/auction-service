91879104332df454673ca5fcccf0756af3cba4b2
Salil Apte <salil.apte99@gmail.com>
2022-07-18 17:11:33 +0530

    initial sample auction creation mechanism

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,18 @@
-// event  -> actual data
-// context -> metadata, can be used by middleware layer
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  // returns current time in YYYY-MM-DDTHH:mm:ss.sssZ format
+  const now = new Date();
+  const auction = {
+    title,
+    status: 'open',
+    createdAt: now.toISOString()
+  };
   // this is the structure of response expected by AWS Lambda
   return {
-    statusCode: 200,
+    // 201--resource created
+    statusCode: 201,
     // response body always needs to be stringified else it will return errors!
-    body: JSON.stringify({ event, context }), //response
+    body: JSON.stringify({ auction }), //response
   };
 }
 
