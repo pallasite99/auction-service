9f243bf2a701e5a5596e937d8e455d994b38299b
Salil Apte <salil.apte99@gmail.com>
2022-07-19 20:14:14 +0530

    use intrinsic functions and custom vars for optimisation

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -15,7 +15,7 @@ async function createAuction(event, context) {
   };
 
   await dynamoDB.put({
-    TableName: 'AuctionsTable',
+    TableName: process.env.AUCTIONS_TABLE_NAME,
     Item: auction,
   }).promise();
   // this is the structure of response expected by AWS Lambda
