4a328e0c63b0075a20d4b829a000845cc02e9270
Salil Apte <salil.apte99@gmail.com>
2022-07-19 14:54:50 +0530

    PUT request changes using DynamoDB config

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,21 +1,30 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+// global scope as static
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   // returns current time in YYYY-MM-DDTHH:mm:ss.sssZ format
   const now = new Date();
   const auction = {
+    id: uuid(),
     title,
     status: 'open',
     createdAt: now.toISOString()
   };
+
+  await dynamoDB.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
   // this is the structure of response expected by AWS Lambda
   return {
     // 201--resource created
     statusCode: 201,
     // response body always needs to be stringified else it will return errors!
-    body: JSON.stringify({ auction }), //response
+    body: JSON.stringify(auction), //response
   };
 }
 
 export const handler = createAuction;
-
-
